ee809a499c591ac3d41b662448bda88720f3b317
stLmpp <gui.stlmpp@hotmail.com>
2020-11-24 07:30:07 -0300

    refactor(auth): added namespace to auth gateway

--- src/auth/auth.gateway.ts
+++ src/auth/auth.gateway.ts
@@ -1,18 +1,24 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class AuthGateway {
+@WebSocketGateway({ namespace: '/auth' })
+export class AuthGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private _logger: Logger = new Logger('AuthGateway');
 
   @WebSocketServer() server!: Server;
 
   sendTokenSteam(uuid: string, token: string, error?: string): void {
-    this.server.emit('logged-steam', { uuid, token, error });
+    this.server.emit('logged-steam', { token, error, uuid });
   }
 
-  afterInit(server: Server): void {
+  afterInit(): void {
     this._logger.log('Init');
   }
 
@@ -20,7 +26,7 @@ export class AuthGateway {
     this._logger.log(`Client disconnected: ${client.id}`);
   }
 
-  handleConnection(client: Socket, ...args: any[]): void {
+  handleConnection(client: Socket): void {
     this._logger.log(`Client connected: ${client.id}`);
   }
 }
