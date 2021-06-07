5a89b0ec18519a26330c4f38f7299750d4b57607
stLmpp <gui.stlmpp@hotmail.com>
2021-06-07 16:47:19 -0300

    - added missing property decorator in the MailQueueEntity

--- src/mail/mail-queue.entity.ts
+++ src/mail/mail-queue.entity.ts
@@ -3,60 +3,79 @@ import { BaseEntity } from '../shared/super/base-entity';
 import { TextEncodingEnum } from './text-encoding.enum';
 import { MailSendDto } from './mail.dto';
 import { coerceArray } from 'st-utils';
+import { Property } from '../mapper/property.decorator';
 
 @Entity()
 export class MailQueue extends BaseEntity {
+  @Property()
   @Column()
   template!: string;
 
+  @Property()
   @Column({ type: 'json' })
   context!: Record<any, any>;
 
+  @Property()
   @Column()
   from!: string;
 
+  @Property()
   @Column({ array: true, type: 'character varying' })
   to!: string[];
 
+  @Property()
   @Column({ array: true, type: 'character varying', nullable: true })
   cc?: string[];
 
+  @Property()
   @Column({ array: true, type: 'character varying', nullable: true })
   bcc?: string[];
 
+  @Property()
   @Column({ nullable: true })
   replyTo?: string;
 
+  @Property()
   @Column({ nullable: true })
   inReplyTo?: string;
 
+  @Property()
   @Column({ nullable: true })
   subject?: string;
 
+  @Property()
   @Column({ nullable: true })
   text?: string;
 
+  @Property()
   @Column({ nullable: true })
   html?: string;
 
+  @Property()
   @Column({ nullable: true })
   sender?: string;
 
+  @Property()
   @Column({ nullable: true })
   raw?: string;
 
+  @Property()
   @Column({ nullable: true, type: 'enum', enum: TextEncodingEnum })
   textEncoding?: TextEncodingEnum;
 
+  @Property()
   @Column({ nullable: true })
   references?: string;
 
+  @Property()
   @Column({ nullable: true })
   encoding?: string;
 
+  @Property()
   @Column({ nullable: true })
   date?: Date;
 
+  @Property()
   @Column({ nullable: true })
   transporterName?: string;
 
