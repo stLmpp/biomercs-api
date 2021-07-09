417580ba0c6efd95c38dd4336b324b51ecf95c51
stLmpp <gui.stlmpp@hotmail.com>
2021-07-09 20:00:51 -0300

    - use the new InjectMapProfile in the Rule

--- src/rule/rule.controller.ts
+++ src/rule/rule.controller.ts
@@ -7,24 +7,30 @@ import { ApiAdmin } from '../auth/api-admin.decorator';
 import { RuleAddDto, RuleUpdateDto, RuleUpsertDto } from './rule.dto';
 import { Params } from '../shared/type/params';
 import { RuleUpsertRemoveInvalidPipe } from './rule-upsert-remove-invalid.pipe';
+import { InjectMapProfile } from '../mapper/inject-map-profile';
+import { Rule } from './rule.entity';
+import { MapProfile } from '../mapper/map-profile';
 
 @ApiTags('Rule')
 @Controller('rule')
 export class RuleController {
-  constructor(private ruleService: RuleService) {}
+  constructor(
+    private ruleService: RuleService,
+    @InjectMapProfile(Rule, RuleViewModel) private mapProfile: MapProfile<Rule, RuleViewModel>
+  ) {}
 
   @ApiAdmin()
   @ApiAuth()
   @Post()
   async add(@Body() dto: RuleAddDto): Promise<RuleViewModel> {
-    return this.ruleService.add(dto);
+    return this.mapProfile.mapPromise(this.ruleService.add(dto));
   }
 
   @ApiAdmin()
   @ApiAuth()
   @Patch(`:${Params.idRule}`)
   async update(@Param(Params.idRule) idRule: string, @Body() dto: RuleUpdateDto): Promise<RuleViewModel> {
-    return this.ruleService.update(idRule, dto);
+    return this.mapProfile.mapPromise(this.ruleService.update(idRule, dto));
   }
 
   @ApiAdmin()
@@ -32,11 +38,11 @@ export class RuleController {
   @ApiBody({ isArray: true, type: RuleUpsertDto, required: true, description: 'Make sure to send all fields' })
   @Post('upsert')
   async upsert(@Body(RuleUpsertRemoveInvalidPipe) dtos: RuleUpsertDto[]): Promise<RuleViewModel[]> {
-    return this.ruleService.upsert(dtos);
+    return this.mapProfile.mapPromise(this.ruleService.upsert(dtos));
   }
 
   @Get()
   async findAll(): Promise<RuleViewModel[]> {
-    return this.ruleService.findAll();
+    return this.mapProfile.mapPromise(this.ruleService.findAll());
   }
 }
