1a355dad56a742bfe082ada169472ab349762e0f
stLmpp <gui.stlmpp@hotmail.com>
2021-07-30 11:23:25 -0300

    - updated dependencies - added a more robust check in the auth-subscriber.ts

--- src/auth/auth-subscriber.ts
+++ src/auth/auth-subscriber.ts
@@ -2,6 +2,14 @@ import { Connection, EntitySubscriberInterface, EventSubscriber, InsertEvent, Up
 import { getRequestContext } from '../async-hooks';
 import { AUTH_USER_CONTEXT_TOKEN } from './auth-user-context-token';
 import { User } from '../user/user.entity';
+import { BaseEntity } from '../shared/super/base-entity';
+
+export function eventEntityHasColumns<T extends UpdateEvent<any> | InsertEvent<any>, K extends keyof BaseEntity>(
+  event: T,
+  columns: K[]
+): event is T & { entity: { [KEY in K]: BaseEntity[KEY] } } {
+  return !!event.entity && columns.every(column => event.metadata.findColumnWithPropertyName(column));
+}
 
 @EventSubscriber()
 export class AuthSubscriber implements EntitySubscriberInterface {
@@ -10,12 +18,16 @@ export class AuthSubscriber implements EntitySubscriberInterface {
   }
 
   beforeUpdate(event: UpdateEvent<any>): Promise<any> | void {
-    event.entity.lastUpdatedBy = getRequestContext<User>(AUTH_USER_CONTEXT_TOKEN)?.id ?? -1;
+    if (eventEntityHasColumns(event, ['lastUpdatedBy'])) {
+      event.entity.lastUpdatedBy = getRequestContext<User>(AUTH_USER_CONTEXT_TOKEN)?.id ?? -1;
+    }
   }
 
   beforeInsert(event: InsertEvent<any>): Promise<any> | void {
-    const idUser = getRequestContext<User>(AUTH_USER_CONTEXT_TOKEN)?.id ?? -1;
-    event.entity.createdBy = idUser;
-    event.entity.lastUpdatedBy = idUser;
+    if (eventEntityHasColumns(event, ['lastUpdatedBy', 'createdBy'])) {
+      const idUser = getRequestContext<User>(AUTH_USER_CONTEXT_TOKEN)?.id ?? -1;
+      event.entity.createdBy = idUser;
+      event.entity.lastUpdatedBy = idUser;
+    }
   }
 }
